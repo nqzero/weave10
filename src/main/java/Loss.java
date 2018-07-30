899644bbaacc450490cc6706fdc8b10af4d63c02
lytles@takashi <srl.source@nqzero.com>
2018-07-30 15:24:15 -0400

    Loss - adjust whitespace to better align line numbers with Win

--- src/main/java/Loss.java
+++ src/main/java/Loss.java
@@ -1,21 +1,15 @@
 // https://stackoverflow.com/questions/18971830/generating-working-invokedynamic-instruction-with-asm
 // https://blog.gypsyengineer.com/en/tech/whats-new-in-java-10-episode-two.html
 // https://docs.oracle.com/javase/10/tools/java.htm  (-XX options, -Xlog, ...)
-
-
-
 import kilim.Fiber;
 import kilim.Pausable;
 import kilim.Task;
-
 public interface Loss {
-    static public final boolean $isWoven = true;
-    
+    static public final boolean $isWoven = true;    
     void execute(Fiber fiber) throws Pausable, Exception;
     default void execute() throws Pausable, Exception {
         Task.errNotWoven();
     }
-
     public static class Fork extends Task {
         Loss body;
         public Fork(Loss body) { this.body = body; }
