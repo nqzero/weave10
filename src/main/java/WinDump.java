7208b337940a4197c3e5d853fea4e63933c89eba
lytles@takashi <srl.source@nqzero.com>
2018-08-05 16:16:42 -0400

    remove kilim dependency from windump for bug report

--- src/main/java/WinDump.java
+++ src/main/java/WinDump.java
@@ -1,15 +1,13 @@
+import java.io.File;
+import java.io.FileOutputStream;
 import org.objectweb.asm.AnnotationVisitor;
-import org.objectweb.asm.Attribute;
-import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.ConstantDynamic;
 import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.Handle;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
-import org.objectweb.asm.TypePath;
 public class WinDump implements Opcodes {
 
 public static byte[] dump () throws Exception {
@@ -19,7 +17,7 @@ FieldVisitor fieldVisitor;
 MethodVisitor methodVisitor;
 AnnotationVisitor annotationVisitor0;
 
-classWriter.visit(V9, ACC_PUBLIC | ACC_ABSTRACT | ACC_INTERFACE, "Win", null, "java/lang/Object", null);
+classWriter.visit(V1_8, ACC_PUBLIC | ACC_ABSTRACT | ACC_INTERFACE, "Win", null, "java/lang/Object", null);
 
 classWriter.visitSource("Win.java", null);
 
@@ -30,13 +28,12 @@ fieldVisitor = classWriter.visitField(ACC_PUBLIC | ACC_FINAL | ACC_STATIC, "$isW
 fieldVisitor.visitEnd();
 }
 {
-methodVisitor = classWriter.visitMethod(ACC_PUBLIC | ACC_ABSTRACT, "execute", "()V", null, new String[] { "kilim/Pausable", "java/lang/Exception" });
+methodVisitor = classWriter.visitMethod(ACC_PUBLIC | ACC_ABSTRACT, "execute", "()V", null, new String[] { "java/lang/Exception" });
 methodVisitor.visitEnd();
 }
 {
-methodVisitor = classWriter.visitMethod(ACC_PUBLIC, "execute", "(Lkilim/Fiber;)V", null, new String[] { "kilim/Pausable", "java/lang/Exception" });
+methodVisitor = classWriter.visitMethod(ACC_PUBLIC, "execute", "(Ljava/lang/Object;)V", null, new String[] { "java/lang/Exception" });
 methodVisitor.visitCode();
-methodVisitor.visitMethodInsn(INVOKESTATIC, "kilim/Task", "errNotWoven", "()V", false);
 methodVisitor.visitInsn(RETURN);
 methodVisitor.visitMaxs(0, 2);
 methodVisitor.visitEnd();
@@ -48,7 +45,7 @@ Label label0 = new Label();
 methodVisitor.visitLabel(label0);
 methodVisitor.visitLineNumber(13, label0);
 methodVisitor.visitVarInsn(ALOAD, 0);
-methodVisitor.visitInvokeDynamicInsn("execute", "([Ljava/lang/String;)LWin;", new Handle(Opcodes.H_INVOKESTATIC, "java/lang/invoke/LambdaMetafactory", "metafactory", "(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodHandle;Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/CallSite;", false), new Object[]{Type.getType("(Lkilim/Fiber;)V"), new Handle(Opcodes.H_INVOKESTATIC, "Win", "lambda$main$0", "([Ljava/lang/String;Lkilim/Fiber;)V", true), Type.getType("(Lkilim/Fiber;)V")});
+methodVisitor.visitInvokeDynamicInsn("execute", "([Ljava/lang/String;)LWin;", new Handle(Opcodes.H_INVOKESTATIC, "java/lang/invoke/LambdaMetafactory", "metafactory", "(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodHandle;Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/CallSite;", false), new Object[]{Type.getType("(Ljava/lang/Object;)V"), new Handle(Opcodes.H_INVOKESTATIC, "Win", "lambda$main$0", "([Ljava/lang/String;Ljava/lang/Object;)V", true), Type.getType("(Ljava/lang/Object;)V")});
 methodVisitor.visitVarInsn(ASTORE, 1);
 Label label1 = new Label();
 methodVisitor.visitLabel(label1);
@@ -62,7 +59,7 @@ methodVisitor.visitMaxs(1, 2);
 methodVisitor.visitEnd();
 }
 {
-methodVisitor = classWriter.visitMethod(ACC_PRIVATE | ACC_STATIC | ACC_SYNTHETIC, "lambda$main$0", "([Ljava/lang/String;Lkilim/Fiber;)V", null, new String[] { "kilim/Pausable", "java/lang/Exception" });
+methodVisitor = classWriter.visitMethod(ACC_PRIVATE | ACC_STATIC | ACC_SYNTHETIC, "lambda$main$0", "([Ljava/lang/String;Ljava/lang/Object;)V", null, new String[] { "java/lang/Exception" });
 methodVisitor.visitCode();
 Label label0 = new Label();
 methodVisitor.visitLabel(label0);
@@ -81,9 +78,8 @@ methodVisitor.visitMaxs(2, 2);
 methodVisitor.visitEnd();
 }
 {
-methodVisitor = classWriter.visitMethod(ACC_PRIVATE | ACC_STATIC | ACC_SYNTHETIC, "lambda$main$0", "([Ljava/lang/String;)V", null, new String[] { "kilim/Pausable", "java/lang/Exception" });
+methodVisitor = classWriter.visitMethod(ACC_PRIVATE | ACC_STATIC | ACC_SYNTHETIC, "lambda$main$0", "([Ljava/lang/String;)V", null, new String[] { "java/lang/Exception" });
 methodVisitor.visitCode();
-methodVisitor.visitMethodInsn(INVOKESTATIC, "kilim/Task", "errNotWoven", "()V", true);
 methodVisitor.visitInsn(RETURN);
 methodVisitor.visitMaxs(0, 1);
 methodVisitor.visitEnd();
@@ -92,4 +88,38 @@ classWriter.visitEnd();
 
 return classWriter.toByteArray();
 }
+    public static void main(String[] args) throws Exception {
+        byte [] bytes = WinDump.dump();
+        new FileOutputStream(new File("Win.class")).write(bytes);
+    }
 }
+
+
+/* 
+    We will review your report and have assigned it an internal review ID : 9056496
+
+    NoClassDefFoundError with abstract lambdas that work in java 8 and 9
+    i use ASM to modify lambdas, targeting a default method instead of the SAM.
+      these lambdas worked in java 8 and 9.
+      running in java 10.0.2 they throw  java.lang.NoClassDefFoundError
+    ubuntu 16.04 with oracle jdk 10.0.2 installed locally, ie downloaded manually from oracle.com
+
+    # save WinDump.java and download asm
+    $java8/bin/javac -cp asm-6.2.jar -d . WinDump.java 
+    $java10/bin/java -cp .:asm-6.2.jar WinDump
+    $java10/bin/java -cp . Win
+
+    successful completion with exit status 0 (and running with java 8 or 9 produces this result)
+
+    Exception in thread "main" java.lang.NoClassDefFoundError: Win$$Lambda$1
+            at Win$$Lambda$1/1612799726.get$Lambda(Unknown Source)
+            at Win.main(Win.java:13)
+    Caused by: java.lang.ClassNotFoundException: Win$$Lambda$1
+            at java.base/jdk.internal.loader.BuiltinClassLoader.loadClass(BuiltinClassLoader.java:582)
+            at java.base/jdk.internal.loader.ClassLoaders$AppClassLoader.loadClass(ClassLoaders.java:190)
+            at java.base/java.lang.ClassLoader.loadClass(ClassLoader.java:499)
+            ... 2 more
+
+    use ASM to modify the classes to target the abstract interface method directly
+
+*/
