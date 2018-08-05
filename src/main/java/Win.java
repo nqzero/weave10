291b205abf334b1b46be0a8332995d34194b157e
lytles@takashi <srl.source@nqzero.com>
2018-08-05 14:21:58 -0400

    demo of working and failing woven SAMs and sample code for bug reports

--- src/main/java/Win.java
+++ src/main/java/Win.java
@@ -10,26 +10,58 @@ import kilim.Task;
 import kilim.Mailbox;
 
 public interface Win {
-    void execute(Fiber fiber) throws Pausable, Exception;
-    default void execute() throws Pausable, Exception {}
+    interface Pow extends Wow {
+        void execute(Fiber fiber) throws Pausable, Exception;
+        default void execute() throws Pausable, Exception {}
+    }
+
+    interface Lambda {
+        void execute(Fiber fiber) throws Pausable, Exception;
+        default void execute() throws Pausable, Exception {}
+    }
+    static void dummy() throws Pausable, Exception {
+        Lambda lambda = dummy -> {
+            Task.sleep(1000);
+            System.out.println("hello world");
+        };
+        lambda.execute();
+    }
+    
+    interface Wow {
+        void execute() throws Pausable, Exception;
+    }
     
     public static class Fork extends Task {
-        Win body;
-        public Fork(Win body) { this.body = body; }
+        Pow body;
+        Wow wow;
+        public Fork(Pow body) { this.body = body; }
+        public Fork(Wow body) { this.wow = body; }
         public void execute() throws Pausable, Exception {
-            body.execute();
+            if (body != null)
+                body.execute();
+            else
+                wow.execute();
         }
     }
     
     public static void main(String[] args) throws Exception {
-        if (kilim.tools.Kilim.trampoline(true,args)) return;
+//        if (kilim.tools.Kilim.trampoline(true,args)) return;
         Mailbox<String> mb = new Mailbox<String>();
-        Win mytask = fiber -> {
-            String s = mb.get();  // mb captured from environment.
-            System.out.println(s);
+        Mailbox<String> mb2 = new Mailbox<String>();
+        Pow mytask = fiber -> {
+            String s = mb.get();
+            System.out.println("win: " + s);
         };
         new Fork(mytask).start();
-        mb.putb("Hello from " + 0);  // mb and fi captured from environment
+        mb.putb("Hello from " + 1);
+        if (args.length > 0) {
+            Wow t2 = () -> {
+                String s = mb2.get();
+                System.out.println("wow: " + s);
+            };
+            new Fork(t2).start();
+            mb2.putb("Hello from " + 2);
+        }
         Task.idledown();
     }
 
